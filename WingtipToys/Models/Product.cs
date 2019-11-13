fbf055143c8497c30d2d72cc5faab5e6a33dc045
Tracy Iverson <1797265543@qq.com>
2019-11-13 17:50:13 +0800

    添加项目文件。

---
+++ WingtipToys/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WingtipToys.Models
+{
+    /// <summary>
+    /// 产品实体类,实体类中的每一个属性指定数据表中的列
+    /// </summary>
+    public class Product
+    {
+        [ScaffoldColumn(false)]//数据批注
+        public int ProductID { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Name")]
+        public string ProductName { get; set; }
+
+        [Required, StringLength(10000), Display(Name = "Product Description"), DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        [Display(Name = "Price")]
+        public double? UnitPrice { get; set; }
+
+        public int? CategoryID { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ WingtipToys/Models/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+namespace WingtipToys.Models
+{
+    /// <summary>
+    /// 上下文类,用于管理实体类,并提供对数据库的数据访问
+    /// </summary>
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("WingtipToys")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
